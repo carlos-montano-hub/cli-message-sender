b0246e67c2067a38ac48a623d6eedefdd5de91c0
carlos <montanoc70@gmail.com>
2022-10-26 10:17:04 -0700

    spring init with camel

---
+++ src/main/java/com/paynalli/climessagesender/CliMessageSenderApplication.java
@@ -0,0 +1,13 @@
+package com.paynalli.climessagesender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliMessageSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliMessageSenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paynalli/climessagesender/CliMessageSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paynalli.climessagesender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CliMessageSenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
